c48119ef70eb3973091634a8fb56d1793ac1b7d9
Vsevolod Strukchinsky <floatdrop@gmail.com>
2014-08-20 20:20:38 +0600

    fix(bh): use blank base and cwd

--- index.js
+++ index.js
@@ -1,29 +1,21 @@
 var through = require('through2');
 var File = require('vinyl');
 var BH = require('bh').BH;
-var path = require('path');
 
 module.exports = function (bemjson, fileName, options) {
     options = options || {};
     var bh = new BH();
-    var firstFile;
 
     function apply(obj, enc, cb) {
-        firstFile = obj;
         require(obj.path)(bh);
         cb(null);
     }
 
     function compile() {
-        if (!firstFile) {
-            firstFile = {
-                base: path.dirname(process.cwd()),
-                cwd: process.cwd()
-            };
-        }
-
         var outputFile = new File({
-            path: path.join(firstFile.base, fileName),
+            base: '',
+            cwd: '',
+            path: fileName,
             contents: new Buffer(bh.apply(bemjson))
         });
 
